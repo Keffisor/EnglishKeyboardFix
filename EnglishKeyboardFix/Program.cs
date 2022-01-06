4dd1370f447c7532c14dc1dc8430779c7fbbc78f
Keffisor21 <38363065+Keffisor@users.noreply.github.com>
2022-01-06 10:21:22 +0100

    -
    -

---
+++ EnglishKeyboardFix/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EnglishKeyboardFix
+{
+    static class Program
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        static void Main()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static bool isCtrlPressed = false;
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys key = (Keys)vkCode;
+
+                if (wParam == (IntPtr)WM_KEYUP)
+                    keyUp(lParam, key.ToString());
+                if (wParam == (IntPtr)WM_KEYDOWN)
+                    if (keyDown(wParam, key.ToString()))
+                        return (IntPtr)1;
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static void keyUp(IntPtr param, string key)
+        {
+            if (key.Equals("LControlKey"))
+                isCtrlPressed = false;
+        }
+
+        public static bool keyDown(IntPtr param, string key)
+        {
+            if (key.Equals("LControlKey"))
+                isCtrlPressed = true;
+
+            if (isCtrlPressed)
+            {
+                if (key.Equals("Oemcomma"))
+                {
+                    string contentBefore = getTextClipboard();
+                    SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
+                    copyClipboard("<");
+                    SendKeys.Send("v");
+                    copyClipboard(contentBefore);
+                    return true;
+                }
+                if (key.Equals("OemPeriod"))
+                {
+                    string contentBefore = getTextClipboard();
+                    SetForegroundWindow(Process.GetCurrentProcess().MainWindowHandle);
+                    copyClipboard(">");
+                    SendKeys.Send("v");
+                    copyClipboard(contentBefore);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static string getTextClipboard()
+        {
+            if (!IsClipboardFormatAvailable(13))
+                return null;
+            if (!OpenClipboard(IntPtr.Zero))
+                return null;
+
+            string data = null;
+            var hGlobal = GetClipboardData(13);
+            if (hGlobal != IntPtr.Zero)
+            {
+                var lpwcstr = GlobalLock(hGlobal);
+                if (lpwcstr != IntPtr.Zero)
+                {
+                    data = Marshal.PtrToStringUni(lpwcstr);
+                    GlobalUnlock(lpwcstr);
+                }
+            }
+            CloseClipboard();
+
+            return data;
+        }
+
+        public static void copyClipboard(string content)
+        {
+            OpenClipboard(IntPtr.Zero);
+            var ptr = Marshal.StringToHGlobalUni(content);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+        }
+
+        // Send key
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+        //
+
+        // Clipboard api
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetClipboardData(uint uFormat);
+
+        [DllImport("user32.dll")]
+        static extern bool IsClipboardFormatAvailable(uint format);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+        //
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+
+}
+
